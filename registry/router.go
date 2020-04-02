a719cb85d0f1fc6c4082f5ef42f1427b381059b4
knqyf263 <knqyf263@gmail.com>
2020-04-02 22:54:03 +0300

    refactor(registry): use google/go-containerregistry

---
+++ registry/router.go
@@ -0,0 +1,136 @@
+package registry
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/docker/docker/api/server/router"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+
+	"github.com/aquasecurity/testdocker/tarfile"
+)
+
+// registryRouter is a router to talk with the image controller
+type registryRouter struct {
+	routes []router.Route
+	images map[string]string
+}
+
+// NewRouter initializes a new image router
+func NewRouter(images map[string]string) router.Router {
+	r := &registryRouter{
+		images: images,
+	}
+	r.initRoutes()
+	return r
+}
+
+// Routes returns the available routes to the image controller
+func (s *registryRouter) Routes() []router.Route {
+	return s.routes
+}
+
+// initRoutes initializes the routes in the image router
+func (s *registryRouter) initRoutes() {
+	s.routes = []router.Route{
+		// GET
+		router.NewGetRoute("/", s.pingHandler),
+		router.NewGetRoute("/{name:.*}/manifests/{reference}", s.manifestHandler),
+		router.NewGetRoute("/{name:.*}/blobs/{digest}", s.blobHandler),
+	}
+}
+
+func (s *registryRouter) pingHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+	switch vars["version"] {
+	case "2":
+		return nil
+	default:
+		w.WriteHeader(http.StatusNotImplemented) // TODO: replace this line with returning error
+		return nil
+	}
+}
+
+func (s *registryRouter) manifestHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+	imageName := fmt.Sprintf("v%s/%s:%s", vars["version"], vars["name"], vars["reference"])
+	filePath, ok := s.images[imageName]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // TODO: replace this line with returning error
+		return nil
+	}
+
+	opener := func() (io.ReadCloser, error) {
+		return tarfile.Open(filePath)
+	}
+
+	img, err := tarball.Image(opener, nil)
+	if err != nil {
+		return err
+	}
+
+	b, err := img.RawManifest()
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(b)
+	return err
+}
+
+func (s *registryRouter) blobHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+	imageName := fmt.Sprintf("v%s/%s", vars["version"], vars["name"])
+	for image, filePath := range s.images {
+		if !strings.HasPrefix(image, imageName) {
+			continue
+		}
+
+		img, err := tarball.ImageFromPath(filePath, nil)
+		if err != nil {
+			return err
+		}
+
+		h, err := v1.NewHash(vars["digest"])
+		if err != nil {
+			return err
+		}
+
+		// return the config file
+		configName, err := img.ConfigName()
+		if err != nil {
+			return err
+		}
+
+		if configName == h {
+			b, err := img.RawConfigFile()
+			if err != nil {
+				return err
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			_, err = w.Write(b)
+			return err
+		}
+
+		// return the layer content
+		l, err := img.LayerByDigest(h)
+		if err != nil {
+		}
+
+		rc, err := l.Compressed()
+		if err != nil {
+			return err
+		}
+
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Docker-Content-Digest", vars["digest"])
+		w.WriteHeader(http.StatusOK)
+		io.Copy(w, rc)
+		return nil
+	}
+
+	w.WriteHeader(http.StatusNotFound) // TODO: replace this line with returning error
+	return nil
+}
--- registry/server.go
+++ registry/server.go
@@ -1,115 +1,17 @@
 package registry
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
+	"net/http/httptest"
 
-	"github.com/aquasecurity/testdocker/tarfile"
-	"github.com/gorilla/mux"
+	"github.com/aquasecurity/testdocker/server"
+	"github.com/docker/docker/api/server/router"
 )
 
-type Registry struct {
-	Images map[string]DockerImageDetail
-}
+func NewDockerRegistry(images map[string]string) *httptest.Server {
+	var routes []router.Router
+	routes = append(routes, NewRouter(images))
 
-func (rg Registry) pingHandler(w http.ResponseWriter, r *http.Request) {
-	switch mux.Vars(r)["apiVersion"] {
-	case "v2":
-		w.WriteHeader(http.StatusOK)
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-	}
-}
+	m := server.CreateMux(routes)
 
-func (rg Registry) manifestHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	imageName := fmt.Sprintf("%s/%s:%s", vars["apiVersion"], vars["name"], vars["reference"])
-	filePath, ok := rg.Images[imageName]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	f, err := tarfile.Open(filePath.imagePath)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	b, err := tarfile.ExtractFileFromTar(f, "manifest.json")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(b)
-}
-
-func containsLayer(input []string, e string) bool {
-	for _, s := range input {
-		if s == e {
-			return true
-		}
-	}
-	return false
-}
-
-func (rg Registry) blobHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	var imageFound bool
-	imageName := fmt.Sprintf("%s/%s", vars["apiVersion"], vars["name"])
-	for image, detail := range rg.Images {
-		if strings.Contains(image, imageName) {
-			if containsLayer(detail.layers, vars["digest"]) {
-				imageFound = true
-			}
-		}
-		if imageFound {
-			f, err := tarfile.Open(detail.imagePath)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			b, err := tarfile.ExtractFileFromTar(f, fmt.Sprintf("%s/layer.tar", vars["digest"]))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Header().Set("Docker-Content-Digest", vars["digest"])
-			w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write(b)
-			return
-		}
-	}
-	if !imageFound {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-}
-
-type DockerImageDetail struct {
-	imagePath string
-	layers    []string
-}
-
-type DockerRegistryStore struct {
-	images map[string]DockerImageDetail
-}
-
-func NewDockerRegistry(detail DockerRegistryStore) *mux.Router {
-	rg := Registry{Images: detail.images}
-
-	r := mux.NewRouter()
-	r.HandleFunc("/{apiVersion}", rg.pingHandler).Methods(http.MethodGet)
-	r.HandleFunc("/{apiVersion}/{name}/manifests/{reference}", rg.manifestHandler).Methods(http.MethodGet)
-	r.HandleFunc("/{apiVersion}/{name}/blobs/{digest}", rg.blobHandler).Methods(http.MethodGet)
-	return r
+	return httptest.NewServer(m)
 }
