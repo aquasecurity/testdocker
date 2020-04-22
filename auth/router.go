de12170cdba723e886c0ccf1c2f1fdb1ff7d3b98
knqyf263 <knqyf263@gmail.com>
2020-04-22 21:39:16 +0300

    feat(auth): support authentication

---
+++ auth/router.go
@@ -0,0 +1,150 @@
+package auth
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/errdefs"
+	"golang.org/x/xerrors"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/docker/docker/api/server/router"
+)
+
+const (
+	issuer = "testdocker"
+)
+
+type authRouter struct {
+	routes []router.Route
+	auth   Auth
+}
+
+type Auth struct {
+	User     string // required
+	Password string // required
+	Secret   string // required
+}
+
+func (a Auth) IsValid() bool {
+	return a.User != "" && a.Password != "" && a.Secret != ""
+}
+
+// NewRouter initializes a new auth router
+func NewRouter(auth Auth) *authRouter {
+	r := &authRouter{
+		auth: auth,
+	}
+	r.initRoutes()
+	return r
+}
+
+// Routes returns the available routes to the image controller
+func (a *authRouter) Routes() []router.Route {
+	return a.routes
+}
+
+// initRoutes initializes the routes in the image router
+func (a *authRouter) initRoutes() {
+	a.routes = []router.Route{
+		// GET
+		router.NewGetRoute("/token", a.tokenHandler), // issue a registry token
+	}
+}
+
+func (a *authRouter) tokenHandler(ctx context.Context, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+	// Authorization: Basic dGVzdDp0ZXN0cGFzcwo=
+	authorization := r.Header.Get("Authorization")
+
+	// Basic dGVzdDp0ZXN0cGFzcwo=
+	s := strings.Fields(authorization)
+	if len(s) != 2 {
+		return errdefs.Unauthorized(xerrors.New("invalid Authorization header"))
+	}
+	if s[0] != "Basic" {
+		return errdefs.Unauthorized(xerrors.New("'Basic' must be specified"))
+	}
+
+	// dGVzdDp0ZXN0cGFzcwo=
+	decoded, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return errdefs.Unauthorized(err)
+	}
+
+	// test:testpass
+	s = strings.Split(string(decoded), ":")
+	if s[0] != a.auth.User || s[1] != a.auth.Password {
+		return errdefs.Unauthorized(xerrors.New("invalid username/password"))
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss": issuer,
+	})
+
+	tokenString, err := token.SignedString([]byte(a.auth.Secret))
+	if err != nil {
+		return err
+	}
+
+	// ref. https://docs.docker.com/registry/spec/auth/token/#requesting-a-token
+	type tokenResponse struct {
+		Token        string    `json:"token"`
+		AccessToken  string    `json:"access_token"`
+		ExpiresIn    int       `json:"expires_in"`
+		IssuedAt     time.Time `json:"issued_at"`
+		RefreshToken string    `json:"refresh_token"`
+	}
+
+	t := tokenResponse{
+		Token:       tokenString,
+		AccessToken: tokenString,
+		ExpiresIn:   60,
+		IssuedAt:    time.Now(),
+	}
+
+	b, _ := json.Marshal(t)
+	if _, err = w.Write(b); err != nil {
+		return errdefs.Unavailable(err)
+	}
+
+	return nil
+}
+
+// Middleware function, which will be called for each request
+func (a authRouter) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Skip checking a token for an endpoint to issue the token
+		if r.Method == http.MethodGet && r.URL.Path == "/token" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			// verify the bearer token
+			_, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, xerrors.New("invalid bearer token")
+				}
+				return []byte(a.auth.Secret), nil
+			})
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		} else {
+			// Write an error and stop the handler chain
+			w.Header().Set("Www-Authenticate", fmt.Sprintf("Bearer realm=http://%s/token", r.Host))
+			http.Error(w, "UNAUTHORIZED", http.StatusUnauthorized)
+		}
+	})
+}
--- registry/server.go
+++ registry/server.go
@@ -3,15 +3,29 @@ package registry
 import (
 	"net/http/httptest"
 
+	"github.com/aquasecurity/testdocker/auth"
 	"github.com/aquasecurity/testdocker/server"
 	"github.com/docker/docker/api/server/router"
 )
 
-func NewDockerRegistry(images map[string]string) *httptest.Server {
+type Option struct {
+	Images map[string]string
+	Auth   auth.Auth
+}
+
+func NewDockerRegistry(option Option) *httptest.Server {
 	var routes []router.Router
-	routes = append(routes, NewRouter(images))
+	routes = append(routes, NewRouter(option.Images))
+
+	a := auth.NewRouter(option.Auth)
+	routes = append(routes, a)
 
 	m := server.CreateMux(routes)
+
+	if option.Auth.IsValid() {
+		// Authentication
+		m.Use(a.Middleware)
+	}
 
 	return httptest.NewServer(m)
 }
