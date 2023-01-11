904af5e5117083a1ef9dc5547bd18244f576a625
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-01-11 11:24:43 +0600

    bump github.com/docker/docker to v23.0.0-rc.1+incompatible

--- server/server.go
+++ server/server.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/docker/docker/api/server/httpstatus"
 	"github.com/docker/docker/api/server/httputils"
 	"github.com/docker/docker/api/server/router"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/versions"
 	"github.com/gorilla/mux"
+	"google.golang.org/grpc/status"
 )
 
 const versionMatcher = "/v{version:[0-9.]+}"
@@ -19,9 +23,29 @@ func makeHTTPHandler(handler httputils.APIFunc) http.HandlerFunc {
 		}
 
 		if err := handler(context.Background(), w, r, vars); err != nil {
-			httputils.MakeErrorHandler(err)(w, r)
+			makeErrorHandler(err)(w, r)
 		}
 	}
+}
+
+func makeErrorHandler(err error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		statusCode := httpstatus.FromError(err)
+		vars := mux.Vars(r)
+		if apiVersionSupportsJSONErrors(vars["version"]) {
+			response := &types.ErrorResponse{
+				Message: err.Error(),
+			}
+			_ = httputils.WriteJSON(w, statusCode, response)
+		} else {
+			http.Error(w, status.Convert(err).Message(), statusCode)
+		}
+	}
+}
+
+func apiVersionSupportsJSONErrors(version string) bool {
+	const firstAPIVersionWithJSONErrors = "1.23"
+	return version == "" || versions.GreaterThan(version, firstAPIVersionWithJSONErrors)
 }
 
 // https://github.com/moby/moby/blob/fdf7f4d4ea38e0af3967668c5e2fd06046b8bead/api/server/server.go#L166
